fce11ac9a3e36e6c44762de7a5c2fd149df0fb41
Christos Chiotis <support@elegrad.com>
2020-11-12 17:03:49 +0200

    Update users-list.php

--- inc/users-list.php
+++ inc/users-list.php
@@ -3,12 +3,13 @@ function DisplayUserList() {
 	$siteusers = get_users( 
 		//[ 'role__in' => [ 'administrator', 'subscriber' ] ] 
 	);
-	echo '<table>';
-	echo '<thead><tr><th>Display Name</th><th>Role</th><th>ID</th></tr></thead>';
+	echo '<table class="wpbf-table wpbf-table-large wpbf-table-hover">';
+	echo '<thead><tr><th>Display Name</th><th>Role</th><th>Email</th><th>ID</th></tr></thead>';
 	foreach ( $siteusers as $user ) {
 		echo '<tr>';
 		echo '<td>' . esc_html( $user->display_name ) . '</td>';
 		echo '<td>' . implode(', ', $user->roles) .'</td>';
+		echo '<td>' . esc_html( $user->user_email ) . '</td>';
 		echo '<td>' . esc_html( $user->ID ) .'</td>';
 		echo '</tr>';
 	}
